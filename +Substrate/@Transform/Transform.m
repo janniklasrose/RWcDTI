9ebeb528f4008b8ee0c11de14f714248ea420ccf
ignasiialemany <59746390+ignasiialemany@users.noreply.github.com>
2021-07-07 15:03:01 +0100

    Fix block transform
    Use proper back and forth transforms to operate in true LOCAL

--- +Substrate/@Transform/Transform.m
+++ +Substrate/@Transform/Transform.m
@@ -7,10 +7,11 @@ classdef Transform
         deg_rot_per_L_in_y = 0
         z_amplitude = 0
         x_frequency = 1
+        shift_block = true
     end
 
     methods
-        [position_LOCAL, position_rotated, fn_RotReverse] = global2local(obj, position)
+        [position_LOCAL, fn_TransformInverse, fn_Rot, fn_RotReverse] = global2local(obj, position)
         [xyz] = local2global(obj, xyz, iX, iY, iZ)
     end
 
